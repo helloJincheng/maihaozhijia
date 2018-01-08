94bbfa0da30ee7bf760d391519e32c6e0ea5d882
zhangjc <1065453629@qq.com>
2018-01-08 22:05:39 +0800

    update

--- js/base.js
+++ js/base.js
@@ -1,14 +1,35 @@
-
 // 搜索
-var nsi=1;
-function topover(){
-    $("#topdiv").show();
-}
-function topout(){
-    $("#topdiv").hide();
-}
-function topjconc(x,y){
-    nsi=x;
-    $("#topnwd").html(y);
-    topout();
-}
+
+
+$(function () {
+
+    var nsi=1;
+    function topover(){
+        $("#topdiv").bind('mouseenter',function () {
+            this.show();
+        })
+    }
+    function topout(){
+        $("#topdiv").hide();
+    }
+    function topjconc(x,y){
+        nsi=x;
+        $("#topnwd").html(y);
+        topout();
+    }
+    
+    
+    
+    
+    
+    $(".btn-sign").hover(function() {
+        var str = $(this).attr("_title");
+        var color = $(this).attr("color");
+        var that = this;
+        layer.tips(str, that, {
+            tips: [1, color]
+        });
+    }, function() {
+        layer.closeAll('tips');
+    });
+})
